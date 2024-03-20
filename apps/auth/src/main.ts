3c1190a480b744f8836abebd5af4baf614392bb7
joe_tian_forexco_home_macmini <j.tian@forexco.com.au>
2024-03-21 00:28:56 +1100

    feat: lecture 15 config refactor

--- apps/auth/src/main.ts
+++ apps/auth/src/main.ts
@@ -2,13 +2,14 @@ import { NestFactory } from '@nestjs/core';
 import { AuthModule } from './auth.module';
 import { ValidationPipe } from '@nestjs/common';
 import { Logger } from 'nestjs-pino';
-
-const HTTP_PORT = process.env.AUTH_PORT;
+import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AuthModule);
   app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
   app.useLogger(app.get(Logger));
-  await app.listen(HTTP_PORT);
+  const configService = app.get(ConfigService);
+  const port = configService.get('PORT');
+  await app.listen(port);
 }
 bootstrap();
--- libs/common/src/database/database.module.ts
+++ libs/common/src/database/database.module.ts
@@ -1,12 +1,10 @@
 import { Module } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { ModelDefinition, MongooseModule } from '@nestjs/mongoose';
-import { ConfigModule } from '../config/config.module';
 
 @Module({
   imports: [
     MongooseModule.forRootAsync({
-      imports: [ConfigModule], // Note: make sure to import the customised one
       inject: [ConfigService],
       useFactory: (configService: ConfigService) => {
         const uriFromEnv = configService.get('MONGODB_URI');
